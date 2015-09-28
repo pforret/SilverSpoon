3f1aa70031141970ded45cbbb53f586b8ab617ad
Brightfish Cinemapub <support@cinemapub.be>
2015-09-28 17:15:27 +0200

    First checkin

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # SilverSpoon
 Set up an API for Raspberry Pi, based on a simple config file
+
+
+## Required files
+
+* SilverSpoon.py - the program
+* config/*.ini - the config files
+
+## Start
+
+* SilverSpoon.py -p [port] [config folder]
---
+++ SilverSpoon.py
@@ -0,0 +1,36 @@
+#!flask/bin/python
+from flask import Flask, jsonify
+import ConfigParser
+import io
+
+
+app = Flask(__name__)
+
+cfile="config/services.ini"
+
+# Load the configuration file
+with open(cfile) as f:
+	sample_config = f.read()
+config = ConfigParser.RawConfigParser(allow_no_value=True)
+config.readfp(io.BytesIO(sample_config))
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+@app.route('/<command>',methods=['GET'])
+def get_command(command):
+	# List all contents
+	print("Reading from [%s]" % command)
+	for section in config.sections():
+		if section == command:
+			for options in config.options(command):
+				print("- %s = %s" % (options,config.get(section, options)))
+	return "Executing [%s]" % command
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
